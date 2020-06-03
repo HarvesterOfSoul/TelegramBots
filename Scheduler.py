a2243de50c9b6a3f008ed4400ff200dc9a4d9df4
TheRealNoob <TheRealNoob@users.noreply.github.com>
2020-06-03 09:18:19 -0500

    randomize seconds

--- Scheduler.py
+++ Scheduler.py
@@ -4,18 +4,18 @@ import asyncio, logging, random
 
 #logging.basicConfig(level=logging.DEBUG)
 
-client = TelegramClient(api_id=,
-                        api_hash='',
+client = TelegramClient(api_id=243333,
+                        api_hash='0449781cff2618594375fb843f401f4a',
                         connection_retries=1000,
-                        session='').start()
+                        session='TheRealNoob').start()
 
 ChatWars_Channel = client.get_entity('@chtwrsbot')
-Control_Channel_URL = 'https://t.me/joinchat/'
+Control_Channel_URL = 'https://t.me/joinchat/Fa7eDBOaineSQqK4Bh6vvA'
 
 async def schedule_arena(days=1):
     for day_count in range(1,days+1):
-        current_time = datetime.now().replace(second=0, microsecond=0)
-        target_time = current_time.replace(hour=4, minute=random.randint(0,59)).astimezone(timezone.utc)
+        current_time = datetime.now().replace(microsecond=0)
+        target_time = current_time.replace(hour=4, minute=random.randint(0,59), second=random.randint(0,59)).astimezone(timezone.utc)
         delta_t = target_time + timedelta(days=day_count)
         await client.send_message(Control_Channel_URL, message="/arena", schedule=delta_t)
         print("Scheduled /arena for {} UTC".format(delta_t))
@@ -24,12 +24,12 @@ async def schedule_arena(days=1):
 
 async def schedule_valley(days=1):
     for day_count in range(1,days+1):
-        current_time = datetime.now().replace(second=0, microsecond=0)
-        target_time1 = current_time.replace(hour=6, minute=random.randint(0,59)).astimezone(timezone.utc)
+        current_time = datetime.now().replace(microsecond=0)
+        target_time1 = current_time.replace(hour=6, minute=random.randint(0,59), second=random.randint(0,59)).astimezone(timezone.utc)
         delta_t1 = target_time1 + timedelta(days=day_count)
-        target_time2 = current_time.replace(hour=14, minute=random.randint(0,59)).astimezone(timezone.utc)
+        target_time2 = current_time.replace(hour=14, minute=random.randint(0,59), second=random.randint(0,59)).astimezone(timezone.utc)
         delta_t2 = target_time2 + timedelta(days=day_count)
-        target_time3 = current_time.replace(hour=21, minute=random.randint(0,59)).astimezone(timezone.utc)
+        target_time3 = current_time.replace(hour=21, minute=random.randint(0,59), second=random.randint(0,59)).astimezone(timezone.utc)
         delta_t3 = target_time3 + timedelta(days=day_count)
 
         await client.send_message(Control_Channel_URL, message="/valley", schedule=delta_t1)
